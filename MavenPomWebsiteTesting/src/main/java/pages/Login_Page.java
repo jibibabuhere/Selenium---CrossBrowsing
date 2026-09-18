d24d6b10a5c753919d919270e9c5a87d41051ba1
JIBI BABU <jibi.babu.here@gmail.com>
2026-09-18 12:20:35 +0530

    Login_Page.java

--- MavenPomWebsiteTesting/src/main/java/pages/Login_Page.java
+++ MavenPomWebsiteTesting/src/main/java/pages/Login_Page.java
@@ -7,20 +7,16 @@ import org.openqa.selenium.support.PageFactory;
 public class Login_Page {
 	
 public WebDriver driver;
-	//Locators using page factory
 	
-	@FindBy(id = "username") WebElement usernameField; //username
-	@FindBy(id = "password") WebElement passwordField; //password
+	@FindBy(id = "username") WebElement usernameField;
+	@FindBy(id = "password") WebElement passwordField;
 	@FindBy(id = "submit") WebElement submitButton;
 	
-	//constructor to initialize page factory elements
 	public Login_Page(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 		
 	}
-	
-	//actions
 	
 	public void enterUserName(String usernmame) {
 		usernameField.sendKeys(usernmame);
